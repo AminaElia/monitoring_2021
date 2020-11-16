4f9e3cd652ac942acef8d717dde711f2224ab0ea
AminaElia <73693978+AminaElia@users.noreply.github.com>
2020-11-16 10:59:17 +0100

    Update R_spatial.r

--- R_spatial.r
+++ R_spatial.r
@@ -26,4 +26,38 @@ bubble(meuse, "lead")
 #changing colour, from green to red
 bubble(meuse, "lead", col="red")
 
+#16 november 2021
+# Installing the ggplot2 library
+
+install.packages("ggplot2")
+library(ggplot2)
+
+# ecological dataframe
+
+# biofules
+biofuels <- c(120, 200, 350, 570, 750) # array of values: c
+# oxydative enzimes
+oydative <- c(1200,1300, 21000,34000, 5000) #errore: oxydative, ho dimenticato la x
+
+#dataframe
+d <- data.frame(biofuels,oydative)
+d # per visualizzare la tabella
+
+ggplot(d, aes(x = biofuels, y = oydative)) + geom_point() # grafico con pallini
+ggplot(d, aes(x = biofuels, y = oydative)) + geom_point(size = 5, col = "red")
+
+ggplot(d, aes(x = biofuels, y = oydative)) + geom_line() # grafico con linea
+
+# grafico con puntini e linee
+ggplot(d, aes(x = biofuels, y = oydative)) + geom_point(size = 5, col = "red") + geom_line()
+
+#polygons
+ggplot(d, aes(x = biofuels, y = oydative)) + geom_polygon()
+
+
+###### IMPORT DATA FROM AN EXTERNAL SOURCE
+# setwd("path/Lab")
+
+ setwd("/Users/Amina/Desktop/Lab")
+
 
