aec4adfb12309663ff84474b9a171d3c68895380
AminaElia <73693978+AminaElia@users.noreply.github.com>
2021-02-10 18:15:13 +0100

    Update R_fortunepalm.r

--- R_fortunepalm.r
+++ R_fortunepalm.r
@@ -1,10 +1,14 @@
 #set working directory
 setwd("/Users/Amina/desktop/Lab")
+
+library(ggplot2)
+
 #import data from external source
-palm <- read.table("fortune2.csv", header=T, sep=",")
+palm <- read.table("palmticino.csv", header=T, sep=",")
 
 summary(palm)
 
 Plot with poit's size corresponding to the number of palms 
 ggplot(palm, aes(x=coorx, y=coory, size=observationspalm)) + geom_point()
 
+ggplot(palm, aes(x=coorx, y=coory, size=observationspalm)) + geom_point(pch=21) + aes(fill=alti)
