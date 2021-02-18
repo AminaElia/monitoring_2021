18d177012117bd2ef71f94af25011902cacc7822
AminaElia <73693978+AminaElia@users.noreply.github.com>
2021-02-18 15:58:24 +0100

    Update R_fortunepalm.r

--- R_fortunepalm.r
+++ R_fortunepalm.r
@@ -1,14 +1,19 @@
+#### Trachycarpus fortunei distribution in Ticino, CH
+
 #set working directory
 setwd("/Users/Amina/desktop/Lab")
 
+# call the package needed
 library(ggplot2)
 
 #import data from external source
 palm <- read.table("palmticino.csv", header=T, sep=",")
 
+# summary to see if data have been correctly imported and to know the names of the columns
 summary(palm)
 
-Plot with poit's size corresponding to the number of palms 
+# Plot with poit's size corresponding to the number of palms 
 ggplot(palm, aes(x=coorx, y=coory, size=observationspalm)) + geom_point()
 
+# Plot with point's size corresponding to the number of palms and the color corresponding to the altitude
 ggplot(palm, aes(x=coorx, y=coory, size=observationspalm)) + geom_point(pch=21) + aes(fill=alti)
